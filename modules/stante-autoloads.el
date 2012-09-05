484b304b04ed6653782c22a4342ad0acd0f28311
Sebastian Wiesner <lunaryorn@gmail.com>
2012-09-05 22:58:57 +0200

    Add generated autoloads file

---
+++ modules/stante-autoloads.el
@@ -0,0 +1,46 @@
+;;; stante-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (stante-update-autoload-file stante-get-file-lines
+;;;;;;  stante-get-file-contents package-install-if-needed) "stante-helper"
+;;;;;;  "stante-helper.el" (20551 48275))
+;;; Generated autoloads from stante-helper.el
+
+(autoload 'package-install-if-needed "stante-helper" "\
+Install the package named NAME, unless it is already installed.
+
+\(fn NAME)" nil nil)
+
+(autoload 'stante-get-file-contents "stante-helper" "\
+Return the contents of the file FILENAME.
+
+\(fn FILENAME)" nil nil)
+
+(autoload 'stante-get-file-lines "stante-helper" "\
+Return a list of lines of file FILENAME.
+
+\(fn FILENAME)" nil nil)
+
+(autoload 'stante-update-autoload-file "stante-helper" "\
+Update the autoload file of Stante Pede.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("stante-editor.el" "stante-emacs-lisp.el"
+;;;;;;  "stante-german.el" "stante-git.el" "stante-osx.el" "stante-programming.el"
+;;;;;;  "stante-ui.el") (20551 48392 427214))
+
+;;;***
+
+(provide 'stante-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; stante-autoloads.el ends here
