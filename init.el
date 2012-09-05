4a4f8f0cbc868725c2f5d23a93ea9da698ae2d5d
Sebastian Wiesner <lunaryorn@gmail.com>
2012-09-05 10:28:59 +0200

    Add Emacs configuration file

---
+++ init.el
@@ -0,0 +1,42 @@
+;;; init.el --- Get your Emacs running, Stante Pede
+;;
+;; Copyright (c) 2012 Sebastian Wiesner
+;;
+;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
+;; URL: https://gihub.com/lunaryorn/stantepede.git
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+
+;;; Commentary:
+
+;; Your personal Emacs configuration.  Load Stante Pede, and choose your modules
+;; wisely.
+
+
+;;; Code:
+
+;; Load Stante Pede
+(load (concat (file-name-directory load-file-name) "stante-init"))
+
+;; Load Stante modules as you like
+(require 'stante-ui)
+
+;;; init.el ends here
