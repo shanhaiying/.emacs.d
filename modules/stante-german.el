877873efe50e8e0930b8fd15b7e9e1475f1c7dae
Sebastian Wiesner <lunaryorn@gmail.com>
2012-09-05 14:42:30 +0200

    Add a module for bindings specific for German layouts

---
+++ modules/stante-german.el
@@ -0,0 +1,49 @@
+;;; stante-german.el --- Stante Pede: Improve experience on German keyboards
+;;
+;; Copyright (c) 2012 Sebastian Wiesner
+;;
+;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
+;; URL: https://gihub.com/lunaryorn/stantepede.git
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+
+;;; Commentary:
+
+;; Improves the experience on German keyboard layouts by re-mapping some
+;; commands so that they are located at a similar place as on US layouts. Note
+;; that this is almost never the exactly same place, because such binding would
+;; shadow useful bindings.
+
+;; Remappings
+;; ----------
+;;
+;; `indent-region' is bound to C-M-ö (standard binding C-M-\).
+;; `hippie-expand' is bound to C-M-ä (standard binding M-/).
+
+
+;;; Code:
+
+(global-set-key (kbd "C-M-ö") 'indent-region)
+(global-set-key (kbd "C-M-ä") 'hippie-expand)
+
+(provide 'stante-german)
+
+;;; stante-editor.el ends here
