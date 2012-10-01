9df3072cda2a65aa6f30823c9cfd384516ae19da
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-01 18:56:02 +0200

    [Fix #15] Reduce priority of LaTeX intermediates in ido
    Move LaTeX intermediates to the end of the file list when finding files with
    IDO.

--- modules/stante-latex.el
+++ modules/stante-latex.el
@@ -112,6 +112,26 @@
 ;; Select best viewing programs
 (after 'tex (stante-TeX-select-view-programs))
 
+;; Move LaTeX intermediates to end
+(defun stante-LaTeX-intermediate-p (filename)
+  "Determine whether FILENAME is an intermediate file."
+  (require 'latex) ; for LaTeX-clean-intermediate-suffixes
+  (let* ((suffixes LaTeX-clean-intermediate-suffixes)
+         (regexp (concat "\\("
+                         (mapconcat 'identity suffixes "\\|")
+                         "\\)\\'")))
+    (string-match-p regexp filename)))
+
+(defun ido-LaTeX-intermediates-to-end ()
+  "Move LaTeX intermediate files to end."
+  (let ((intermediates
+         (delq nil (mapcar (lambda (x)
+                             (when (stante-LaTeX-intermediate-p x) x))
+                           ido-temp-list))))
+    (ido-to-end intermediates)))
+
+(after 'ido
+  (add-hook 'ido-make-file-list-hook 'ido-LaTeX-intermediates-to-end))
 
 ;; Configure RefTeX
 (after 'reftex
