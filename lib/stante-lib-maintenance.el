74cba1cef33ea487f5fe67e123c5644e9ad10284
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-03 16:55:06 +0200

    [Fix #21] Add function to report a Stante Pede issue

--- lib/stante-lib-maintenance.el
+++ lib/stante-lib-maintenance.el
@@ -33,12 +33,16 @@
 ;; Pede.
 ;;
 ;; `stante-byte-recompile' byte-compiles all Stante Pede modules.
+;;
+;; `stante-report-issue' reports an issue to the Stante Pede issue tracker.
 
 ;; Load `stante-lib-autoloads' to use the functions of this library.
 
 ;;; Code:
 
 (require 'autoload)
+(require 'stante-lib-package)
+(package-require 'gh)
 
 ;;;###autoload
 (defconst stante-autoload-file (concat stante-lib-dir "stante-lib-autoloads.el")
@@ -58,6 +62,80 @@
   (byte-recompile-directory stante-lib-dir 0)
   (byte-recompile-directory stante-modules-dir 0)
   (byte-recompile-file stante-init-file nil 0))
+
+(defvar stante-report-issue-title nil
+  "The title of
+ the issue to report with `stante-report-issue'.")
+
+(defvar stante-report-issue-gh-api nil
+  "The Github API object to use for `stante-report-issue'.")
+
+(defun stante-report-issue-new (title body)
+  "Create a new Stante Pede issue with given TITLE and BODY.
+
+The issue is submitted to the Github issue tracker of Stante
+Pede.  See `stante-issues-url'."
+  (unless stante-report-issue-gh-api
+    (setq stante-report-issue-gh-api (gh-issues-api "Stante Pede issues API")))
+  (let ((issue (make-instance 'gh-issues-issue
+                              :body body :title title :state "open")))
+    (gh-issues-issue-new stante-report-issue-gh-api
+                         "lunaryorn" "stante-pede"
+                         issue))
+  (stante-report-issue-cancel))
+
+(defun stante-report-issue-submit ()
+  "Submit the issue."
+  (interactive)
+  (when stante-report-issue-title
+    (let ((title stante-report-issue-title)
+          (body (buffer-substring-no-properties (point-min) (point-max))))
+      (if (string= "" body)
+          (if (y-or-n-p "The issue comment is empty. Submit anyway?")
+            (stante-report-issue-new title body)
+            (message "Not submitted, the comment is empty."))
+        (stante-report-issue-new title body)))))
+
+(defun stante-report-issue-cancel ()
+  "Abort edits and erase the issue."
+  (interactive)
+  (when stante-report-issue-title
+    (set-buffer-modified-p nil)
+    (erase-buffer)
+    (kill-buffer)))
+
+;;;###autoload
+(defun stante-report-issue (title)
+  "Report an issue with TITLE to Stante Pede.
+
+If called interactively, prompt for TITLE.
+
+Pop up a buffer to edit the issue comment.  If `gfm-mode' is
+available, use it in this buffer, otherwise fall back to
+`text-mode'.
+
+In this buffer, use C-c C-c s to submit the issue,
+and C-c C-c k to cancel the process. "
+  (interactive "sTitle of this issue: ")
+  (let ((issue-buffer (get-buffer-create
+                       (format "*Stante Pede issue: %s*" title))))
+    (with-current-buffer issue-buffer
+      ;; Enable gfm-mode if available, otherwise fall back to standard text-mode
+      (if (fboundp 'gfm-mode)
+          (gfm-mode)
+        (message "Add (require 'stante-markdown) for Markdown support in the \
+issue comment.")
+        (text-mode))
+      (set (make-local-variable 'stante-report-issue-title) title)
+      (local-set-key (kbd "C-c C-c s") 'stante-report-issue-submit)
+      (local-set-key (kbd "C-c C-c k") 'stante-report-issue-cancel)
+      ;; Disable saving
+      (local-set-key (kbd "C-x C-s")
+                     (lambda ()
+                       (interactive)
+                       (message "Not saved. Use C-c C-c s to submit this issue \
+or C-c C-c k to cancel."))))
+    (pop-to-buffer issue-buffer)))
 
 (provide 'stante-lib-maintenance)
 
