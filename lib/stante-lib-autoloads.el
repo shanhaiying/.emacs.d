030cf537069dea9a09c06073efbe70e07c62ea10
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-03 17:13:43 +0200

    Update the autoloads

--- lib/stante-lib-autoloads.el
+++ lib/stante-lib-autoloads.el
@@ -67,9 +67,9 @@ FILE may be a named feature, see `eval-after-load'.
 
 ;;;***
 
-;;;### (autoloads (stante-byte-recompile stante-update-autoload-file)
-;;;;;;  "stante-lib-maintenance" "stante-lib-maintenance.el" (20568
-;;;;;;  48986))
+;;;### (autoloads (stante-report-issue stante-byte-recompile stante-update-autoload-file)
+;;;;;;  "stante-lib-maintenance" "stante-lib-maintenance.el" (20588
+;;;;;;  21919))
 ;;; Generated autoloads from stante-lib-maintenance.el
 
 (defconst stante-autoload-file (concat stante-lib-dir "stante-lib-autoloads.el") "\
@@ -84,6 +84,20 @@ Update the autoload file of Stante Pede.
 Byte-compile all modules of Stante pede.
 
 \(fn)" t nil)
+
+(autoload 'stante-report-issue "stante-lib-maintenance" "\
+Report an issue with TITLE to Stante Pede.
+
+If called interactively, prompt for TITLE.
+
+Pop up a buffer to edit the issue comment.  If `gfm-mode' is
+available, use it in this buffer, otherwise fall back to
+`text-mode'.
+
+In this buffer, use C-c C-c s to submit the issue,
+and C-c C-c k to cancel the process.
+
+\(fn TITLE)" t nil)
 
 ;;;***
 
@@ -103,14 +117,19 @@ homebrew.  In future, more sophisticated logic might be added.
 
 ;;;***
 
-;;;### (autoloads (package-need) "stante-lib-package" "stante-lib-package.el"
-;;;;;;  (20587 17588))
+;;;### (autoloads (package-require package-need) "stante-lib-package"
+;;;;;;  "stante-lib-package.el" (20588 21919))
 ;;; Generated autoloads from stante-lib-package.el
 
 (autoload 'package-need "stante-lib-package" "\
 Ensure that the package NAME is available.
 
 If the package is not available, it is automatically installed.
+
+\(fn NAME)" nil nil)
+
+(autoload 'package-require "stante-lib-package" "\
+Ensure that package NAME is available and require the feature NAME.
 
 \(fn NAME)" nil nil)
 
@@ -156,8 +175,14 @@ Refresh imenu and jump to a place in the buffer using Ido.
 
 ;;;***
 
+
 ;;;### (autoloads nil nil ("stante-lib-TeX-biber.el" "stante-lib-TeX-latexmk.el")
 ;;;;;;  (20588 11435 623680))
+
+;;;***
+
+;;;### (autoloads nil nil ("stante-lib-TeX-biber.el" "stante-lib-TeX-latexmk.el")
+;;;;;;  (20588 22033 515836))
 
 ;;;***
 
