shanhaiying/.emacs.d

commit 1ddc3ea302330acf974995ca4d4a8362c42ff348

Replace custom whitespace modes with whitespace-cleanup-mode
See https://github.com/purcell/whitespace-cleanup-mode

init.el
@@ -586,6 +586,9 @@ point reaches the beginning or end of the buffer, stop there."
 ;; Make Tab complete if the line is indented
 (setq tab-always-indent 'complete)
 
+;; Indicate empty lines at the end of a buffer in the fringe
+(setq indicate-empty-lines t)
+
 ;; Highlight bad whitespace
 (stante-after whitespace
   (diminish 'whitespace-mode "␣")
@@ -593,36 +596,15 @@ point reaches the beginning or end of the buffer, stop there."
   ;; portions of lines via faces.  Also indicate tabs via characters
   (setq whitespace-style '(face tabs tab-mark empty trailing lines-tail)
         whitespace-line-column nil))    ; Use `fill-column' for overlong lines
-;; Indicate empty lines at the end of a buffer in the fringe
-(setq indicate-empty-lines t)
 
-;; Rigidly cleanup whitespace in programming modes
-(define-minor-mode stante-prog-whitespace-mode
-  "Minor mode to highlight and cleanup whitespace."
-  :lighter nil
-  :keymap nil
-  (cond
-   (stante-prog-whitespace-mode
-    (whitespace-mode 1)
-    (add-hook 'before-save-hook 'whitespace-cleanup nil :local))
-   (:else
-    (whitespace-mode -1)
-    (remove-hook 'before-save-hook 'whitespace-cleanup :local))))
-(add-hook 'prog-mode-hook 'stante-prog-whitespace-mode)
+;; Clean up whitespace
+(stante-after whitespace-cleanup-mode
+  ;; Always clean up whitespace
+  (setq whitespace-cleanup-mode-only-if-initially-clean nil))
 
-;; In plain text, only cleanup trailing whitespace
-(define-minor-mode stante-text-whitespace-mode
-  "Minor mode to highlight and cleanup whitespace."
-  :lighter nil
-  :keymap nil
-  (cond
-   (stante-text-whitespace-mode
-    (whitespace-mode 1)
-    (add-hook 'before-save-hook 'delete-trailing-whitespace nil :local))
-   (:else
-    (whitespace-mode -1)
-    (remove-hook 'before-save-hook 'delete-trailing-whitespace :local))))
-(add-hook 'text-mode-hook 'stante-text-whitespace-mode)
+(--each '(prog-mode-hook text-mode-hook)
+  (add-hook it #'whitespace-mode)
+  (add-hook it #'whitespace-cleanup-mode))
 
 ;; A function to disable highlighting of long lines in modes
 (stante-after whitespace

Cask
@@ -6,7 +6,7 @@
 (depends-on "dash")
 (depends-on "s")
 (depends-on "f")
-(depends-on "epl")
+(depends-on "epl")                      ; Package management
 
 (depends-on "exec-path-from-shell")     ; Fix environment
 
@@ -28,6 +28,7 @@
 (depends-on "launch")                   ; Open files externally
 
 ;; Editing utilities
+(depends-on "whitespace-cleanup-mode")  ; Cleanup whitespace on save
 (depends-on "drag-stuff")               ; Drag stuff around
 (depends-on "fill-column-indicator")    ; Indicate fill column
 (depends-on "volatile-highlights")      ; Highlight editing operations

commit 968cdb38f28315a9ce58b21b94ed3e160f60d536

Fix call of non-existing function
Regression from 1ddc3ea

init.el
@@ -1011,7 +1011,8 @@ suitable processor was found."
   ;; YAML is kind of a mixture between text and programming language, and hence
   ;; derives from `fundamental-mode', so we enable a good mixture of our hooking
   ;; explicitly
-  (--each '(stante-text-whitespace-mode
+  (--each '(whitespace-mode
+            whitespace-cleanup-mode
             stante-auto-fill-comments-mode
             fci-mode
             flyspell-prog-mode)