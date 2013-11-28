3f070fdd275487540a7a2fcf085e6d1bcc07a35f
Sebastian Wiesner <lunaryorn@gmail.com>
2013-11-28 23:02:17 +0100

    Tune Magit

--- init.el
+++ init.el
@@ -1336,7 +1336,11 @@ keymap `stante-smartparens-lisp-mode-map'."
         magit-stage-all-confirm nil
         magit-unstage-all-confirm nil
         ;; Except when you ask something useful…
-        magit-set-upstream-on-push t)
+        magit-set-upstream-on-push t
+        ;; Show me the changes when committing
+        magit-expand-staged-on-commit 'full
+        ;; Use IDO for completion
+        magit-completing-read-function #'magit-ido-completing-read)
 
   ;; Update Diff highlighting after Magit operations
   (add-hook 'magit-refresh-file-buffer-hook #'diff-hl-update))
