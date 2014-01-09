571e9d9a6cc341d02b69f54c79d1cb252290ab3e
Sebastian Wiesner <lunaryorn@gmail.com>
2014-01-09 14:28:05 +0100

    Replace Inferior Haskell Mode with Interactive Haskell Mode
    The latter takes Cabal settings into account, and supports Cabal
    sandboxes

--- init.el
+++ init.el
@@ -1649,7 +1649,16 @@ Create a new ielm process if required."
 
 (stante-after haskell-mode
   (define-key haskell-mode-map (kbd "C-c h") #'haskell-hoogle)
-  (define-key haskell-mode-map (kbd "C-c f c") #'haskell-cabal-visit-file))
+  (define-key haskell-mode-map (kbd "C-c f c") #'haskell-cabal-visit-file)
+  ;; Replace Inferior Haskell Mode with Interactive Haskell Mode
+  (define-key haskell-mode-map (kbd "C-x C-d") nil)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+  (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c M-.") nil)
+  (define-key haskell-mode-map (kbd "C-c C-d") nil))
 
 ;; Local Variables:
 ;; coding: utf-8
