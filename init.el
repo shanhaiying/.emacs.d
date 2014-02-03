b89892734ca1b8c82c6339df4653ee392491bcd4
Sebastian Wiesner <lunaryorn@gmail.com>
2014-02-03 16:47:54 +0100

    Replace just-one-space with cycle-spacing

--- init.el
+++ init.el
@@ -1664,6 +1664,7 @@ Create a new ielm process if required."
 (global-set-key [remap dabbrev-expand] #'hippie-expand)
 (global-set-key [remap isearch-forward] #'isearch-forward-regexp)
 (global-set-key [remap isearch-backward] #'isearch-backward-regexp)
+(global-set-key [remap just-one-space] #'cycle-spacing)
 ;; Complement standard bindings (the comments indicate the related bindings)
 (global-set-key (kbd "M-X") #'smex-major-mode-commands)                  ; M-x
 (global-set-key (kbd "C-<backspace>") #'stante-smart-backward-kill-line) ; C-S-backspace
