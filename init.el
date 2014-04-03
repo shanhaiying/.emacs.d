eaf28827015e0dbc8aa570a315ccd4ee1ed4a564
Sebastian Wiesner <lunaryorn@gmail.com>
2014-04-03 13:31:50 +0200

    Limit colorization to Compile Mode buffers
    Prevents conflicts with Ag, Grep and friends

--- init.el
+++ init.el
@@ -1052,8 +1052,9 @@ suitable processor was found."
 
 Taken from http://stackoverflow.com/a/3072831/355252."
   (interactive)
-  (let ((inhibit-read-only t))
-    (ansi-color-apply-on-region (point-min) (point-max))))
+  (when (eq major-mode 'compilation-mode)
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region (point-min) (point-max)))))
 
 (stante-after compile
   (setq compilation-ask-about-save nil  ; Just save before compiling
