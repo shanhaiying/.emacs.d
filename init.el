860c23db7def033f47d8f808f1d487d4de254dfc
Sebastian Wiesner <lunaryorn@gmail.com>
2014-04-04 15:13:47 +0200

    Show current function name in header line

--- init.el
+++ init.el
@@ -1047,6 +1047,15 @@ suitable processor was found."
 ;; Folding and heading navigation
 (add-hook 'prog-mode-hook #'outline-minor-mode)
 
+;; Show the current function name in the header line
+(which-function-mode)
+(setq-default header-line-format
+              '((which-func-mode ("" which-func-format " "))))
+(setq mode-line-misc-info
+      ;; We remove Which Function Mode from the mode line, because it's mostly
+      ;; invisible here anyway.
+      (assq-delete-all 'which-func-mode mode-line-misc-info))
+
 ;; Compilation from Emacs
 (defun stante-colorize-compilation-buffer ()
   "Colorize a compilation mode buffer.
