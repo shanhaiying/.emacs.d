6c5b620a03f9bf525c36ee882d5b7fd12bda028d
Sebastian Wiesner <lunaryorn@gmail.com>
2014-04-07 12:00:11 +0200

    Make simplezen available in Markdown Mode

--- init.el
+++ init.el
@@ -1680,6 +1680,9 @@ Create a new ielm process if required."
   (define-key html-mode-map (kbd "C-c e") #'simplezen-expand)
   (define-key html-mode-map (kbd "TAB") #'simplezen-expand-or-indent-for-tab))
 
+(stante-after markdown-mode
+  (define-key markdown-mode-map (kbd "C-c e") #'simplezen-expand))
+
 (stante-after haskell-mode
   (define-key haskell-mode-map (kbd "C-c h") #'haskell-hoogle)
   (define-key haskell-mode-map (kbd "C-c f c") #'haskell-cabal-visit-file)
