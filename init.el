1a437c6fb053d7d9b401040ab38a2d2ced6874f4
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-07-24 22:44:39 +0200

    Show current function in mode-line
    Saves space of the header line

--- init.el
+++ init.el
@@ -1280,12 +1280,6 @@ Choose Skim if available, or fall back to the default application."
 (which-function-mode)
 (lunaryorn-after which-func
   (setq which-func-unknown "⊤"))
-(setq-default header-line-format
-              '((which-func-mode ("" which-func-format " "))))
-(setq mode-line-misc-info
-      ;; We remove Which Function Mode from the mode line, because it's mostly
-      ;; invisible here anyway.
-      (assq-delete-all 'which-func-mode mode-line-misc-info))
 
 ;; Compilation from Emacs
 (defun lunaryorn-colorize-compilation-buffer ()
