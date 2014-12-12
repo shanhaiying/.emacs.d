4763e0aca93a1b6f6f78a2c24d20eca773980efd
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-12-12 15:29:24 +0100

    Add Structured Haskell Mode to the mode line

--- init.el
+++ init.el
@@ -510,8 +510,12 @@ mouse-3: go to end")
                 mode-line-remote
                 mode-line-frame-identification
                 mode-line-buffer-identification " " mode-line-position
-                ;; Some specific information about the current buffer:
+                ;; Some specific information about the current buffer:  Indicate
+                ;; the presence of structured editing, with Paredit or SHM
                 (paredit-mode (:propertize " ()" face bold))
+                (structured-haskell-mode (:propertize shm-lighter face bold))
+                (structured-haskell-repl-mode (:propertize shm-lighter
+                                                           face bold))
                 ;; Warn if whitespace isn't highlighted or cleaned in this
                 ;; buffer.
                 (:eval (unless buffer-read-only
