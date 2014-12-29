a0f8a91f55fdc9a9bcd48eb1c865e30e6e9f9bf4
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-12-29 11:34:39 +0100

    Configure info after it was loaded

--- init.el
+++ init.el
@@ -100,9 +100,10 @@
     ;; Re-initialize the `Info-directory-list' from $INFOPATH.  Since package.el
     ;; already initializes info, we need to explicitly add the $INFOPATH
     ;; directories to `Info-directory-list'.
-    (dolist (dir (parse-colon-path (getenv "INFOPATH")))
-      (when dir
-        (add-to-list 'Info-directory-list dir)))))
+    (with-eval-after-load 'info
+      (dolist (dir (parse-colon-path (getenv "INFOPATH")))
+        (when dir
+          (add-to-list 'Info-directory-list dir))))))
 
 
 ;;; Customization interface
