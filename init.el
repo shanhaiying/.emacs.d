ae715ba2a3abde27625c39ca67c0c54d9aca0f2e
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-01-05 23:56:59 +0100

    Fix binding for copyright-update

--- init.el
+++ init.el
@@ -477,7 +477,7 @@ mouse-3: go to end"))))
 
 (use-package copyright                  ; Deal with copyright notices
   :defer t
-  :bind (("C-c u c" . copyright-update))
+  :bind (("C-c u C" . copyright-update))
   ;; Update copyright when visiting files
   :init (add-hook 'find-file-hook #'copyright-update))
 
